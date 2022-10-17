18980f4a9d7050655375141b31d385139022ddbe
hongyoudan <2422736288@qq.com>
2022-10-17 14:14:46 +0800

    init project

---
+++ src/main/java/com/hyd/breakpointresume/BreakpointResumeApplication.java
@@ -0,0 +1,13 @@
+package com.hyd.breakpointresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakpointResumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreakpointResumeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyd/breakpointresume/BreakpointResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyd.breakpointresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakpointResumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
